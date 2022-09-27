27b4e10b6cc0ba61161d2175d131058b2f074a28
neo-ait <neo.wang@aitsolution.ca>
2022-09-27 14:10:21 -0400

    tabbar

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="tab-bar-item" @click="itemClick">
+    
+    <slot v-if="!isActive" name="item-icon"></slot>
+    
+    <slot v-else name="item-icon-active"></slot>
+    <div :style="isStyleActive">
+      <slot name="item-text"></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "TabBarItem",
+        props: {
+            path: {
+                type: String,
+            },
+            activeStyle: {
+                type: String,
+                default: 'coral'
+            }
+        },
+        data () {
+            return {
+            }
+        },
+        methods: {
+            itemClick() {
+                this.$router.replace(this.path)
+            }
+        },
+        computed: {
+            isActive() {
+                return this.$route.path.indexOf(this.path) !== -1
+            },
+            isStyleActive() {
+                return this.isActive ? {color:this.activeStyle} : {}
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+.tab-bar-item {
+    flex: 1;
+    text-align: center;
+    height: 49px;
+    font-size: 14px;
+
+  }
+
+  .tab-bar-item img{
+    width: 24px;
+    height: 24px;
+    margin-top: 3px;
+    vertical-align: middle;
+    margin-bottom: 2px;
+  }
+
+  .actived {
+    color:coral;
+    font-weight: bolder;
+  }
+
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,45 @@
+<template>
+  
+
+  <tab-bar>
+    
+    <tab-bar-item path="/home" >
+      
+      <!--dom 中使用路径需要用 ~ 来识别配置的别名-->
+      <img slot="item-icon" src="~assets/imgs/tabbar/home.png" alt="">
+      <img slot="item-icon-active" src="~assets/imgs/tabbar/home-active.png" alt="">
+      <div slot="item-text">Home</div>
+    </tab-bar-item>
+    <tab-bar-item path="/category">
+      <img slot="item-icon" src="~assets/imgs/tabbar/category.png" alt="">
+      <img slot="item-icon-active" src="~assets/imgs/tabbar/category-active.png" alt="">
+      <div slot="item-text">Cate</div>
+    </tab-bar-item>
+    <tab-bar-item path="/cart">
+      <img slot="item-icon" src="~assets/imgs/tabbar/cart.png" alt="">
+      <img slot="item-icon-active" src="~assets/imgs/tabbar/cart-active.png" alt="">
+      <div slot="item-text">Cart</div>
+    </tab-bar-item>
+    <tab-bar-item path="/profile">
+      <img slot="item-icon" src="~assets/imgs/tabbar/profile.png" alt="">
+      <img slot="item-icon-active" src="~assets/imgs/tabbar/profile-active.png" alt="">
+      <div slot="item-text">Profile</div>
+    </tab-bar-item>
+  </tab-bar>
+</template>
+
+<script>
+    import TabBar from 'components/common/tabbar/TabBar'
+    import TabBarItem from 'components/common/tabbar/TabBarItem'
+
+    export default {
+        name: "MainTabBar",
+        components: {
+            TabBar,TabBarItem
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
